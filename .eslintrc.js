328ed12cba26755024412fd0e6290d2bb4c3fdc0
Yang Wentao <xat_massacre@Yang.local>
2017-03-05 02:46:46 +0800

    Add eslint to normalize code style

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    'browser': true,
+    'node': true
+  },
+  parser: 'babel-eslint',
+  // you could use airbnb or standard,
+  // extends: 'airbnb',
+  // add your custom rules here
+  'rules': {
+
+  }
+}
